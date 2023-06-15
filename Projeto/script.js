5d7d467cf0dc61ed2d1480d8c51bc473de18b597
Igor Franco <igorfranco@Igors-MacBook-Pro.local>
2023-06-15 11:34:43 -0300

    Projeto1.0

--- Projeto/script.js
+++ Projeto/script.js
@@ -5,7 +5,7 @@ var prazo = 84;
 var seguroPrestamista = 0.12782
 var parcela;
 
-function calcularEmprestimo() {
+function calcularEmprestimo() { //formula para calcular emprestimo baseado no margem consignavel máxima 
     parcela = aposentadoria * margem;
     var emprestimo = parcela * (1 - Math.pow(1 + juros, -prazo)) / juros;
 
@@ -15,35 +15,28 @@ function calcularEmprestimo() {
         valorSeguro = emprestimo * seguroPrestamista; // calcula o valor do seguro
     }
     
-    
-    // var iofDiario = 0.0082 / 100;
-    // var iofFixo = 0.38 / 100;
-    // var iofDiario = 0.0011182 / 100;
-    // var iofFixo = 0.38 / 100;
-    // var iofTotal = emprestimo * (iofDiario * 365 + iofFixo);
-    
     var iofDiario = 0.0001182; // 0.01182% em termos decimais
     var iofFixo = 0.0038; // 0.38% em termos decimais
-    var prazoEmDias = Math.min(prazo * 30, 365);
+    var prazoEmDias = Math.min(prazo * 30, 365);// maximo de 365 dias
+     
 
-    //var iofTotal = emprestimo * (iofDiario * prazoEmDias + iofFixo);
-    var iofTotal = emprestimo * (iofDiario * prazoEmDias);
+    var iofTotal = emprestimo * (iofDiario * prazoEmDias + iofFixo);
 
 
     var emprestimoLiquido = emprestimo - iofTotal - valorSeguro;
-    var valorTotal = parcela * prazo; // Novo cálculo
+    var valorTotal = parcela * prazo; 
 
     return {
         emprestimo: emprestimo,
         iof: iofTotal,
         liquido: emprestimoLiquido,
-        parcela: parcela, // Novo campo
+        parcela: parcela, 
         total: valorTotal,  
         seguro: valorSeguro
     };
 }
 
-function formatarMoeda(input) {
+function formatarMoeda(input) { // mascara de moeda
     var valor = input.value;
     valor = valor.replace(/\D/g, "");
     valor = (valor / 100).toFixed(2);
@@ -52,22 +45,23 @@ function formatarMoeda(input) {
     input.value = valor;
 }
 
-function converterParaNumero(valor) {
+function converterParaNumero(valor) { // converte texto em numero
+    
     valor = valor.replace("R$", "");
     valor = valor.replace(",", ".");
     return parseFloat(valor);
 }
 
-function atualizarInterface(elementId, results) {
+function atualizarInterface(elementId, results) { // imprime dados na tela formatados
     var resultHtml = 
-    "Valor do empréstimo: R$ " + results.emprestimo.toLocaleString('pt-BR', {minimumFractionDigits: 2}) + 
-    "<br style='margin:10px 0;'>Valor do IOF: R$ " + results.iof.toLocaleString('pt-BR', {minimumFractionDigits: 2}) + 
-    "<br style='margin:10px 0;'>Crédito líquido ao cliente: R$ " + results.liquido.toLocaleString('pt-BR', {minimumFractionDigits: 2}) + 
-    "<br style='margin:10px 0;'>Valor da parcela (Prestação ): R$ " + results.parcela.toLocaleString('pt-BR', {minimumFractionDigits: 2}) +  
-    "<br style='margin:10px 0;'>Valor total das parcelas: R$ " + results.total.toLocaleString('pt-BR', {minimumFractionDigits: 2});  
+    "Valor do empréstimo: R$ " + results.emprestimo.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + 
+    "<br style='margin:10px 0;'>Valor do IOF: R$ " + results.iof.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + 
+    "<br style='margin:10px 0;'>Crédito líquido ao cliente: R$ " + results.liquido.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + 
+    "<br style='margin:10px 0;'>Valor da parcela (Prestação ): R$ " + results.parcela.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2}) +  
+    "<br style='margin:10px 0;'>Valor total das parcelas: R$ " + results.total.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2});  
 
     if (results.seguro > 0) {
-        resultHtml += "<br style='margin:10px 0;'>Valor do seguro: R$ " + results.seguro.toLocaleString('pt-BR', {minimumFractionDigits: 2}); // adiciona o valor do seguro se ele for maior que zero
+        resultHtml += "<br style='margin:10px 0;'>Valor do seguro: R$ " + results.seguro.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2}); // adiciona o valor do seguro se ele for maior que zero
     }
 
     document.getElementById(elementId).innerHTML = resultHtml;
@@ -77,10 +71,11 @@ function atualizarInterface(elementId, results) {
 
 
 
-function calcular() {
+function calcular() { // função para calcular o valor do empréstimo
+    document.getElementById("error").innerHTML = ""; // Adicionado para limpar a mensagem de erro
     aposentadoria = converterParaNumero(document.getElementById("aposentadoria").value);
 
-    if (aposentadoria <= 0) {
+    if (aposentadoria <= 0 || isNaN(aposentadoria)) {
         document.getElementById("error").innerHTML = "Por favor, insira um valor de aposentadoria válido.";
         return;
     }
@@ -90,9 +85,10 @@ function calcular() {
 }
 
 
-var prazoRecalculado; // nova variável
+var prazoRecalculado;
 
-function calcularEmprestimoValorFixo(valorParcela, prazoAtual) {
+function calcularEmprestimoValorFixo(valorParcela, prazoAtual) { // função para calcular o valor do empréstimo baseado no valor da parcela e o prazo
+    
     var emprestimo = valorParcela * (1 - Math.pow(1 + juros, -prazoAtual)) / juros;
 
     var seguro = document.getElementById("seguro").checked; // pega o valor do checkbox
@@ -100,17 +96,12 @@ function calcularEmprestimoValorFixo(valorParcela, prazoAtual) {
     if (seguro) {
         valorSeguro = emprestimo * seguroPrestamista; // calcula o valor do seguro
     }
-
-    // var iofDiario = 0.0082 / 100;
-    // var iofFixo = 0.38 / 100;
-    // var iofTotal = emprestimo * (iofDiario * prazoAtual + iofFixo);
     
     var iofDiario = 0.0001182; // 0.01182% em termos decimais
     var iofFixo = 0.0038; // 0.38% em termos decimais
-    var prazoEmDias = Math.min(prazo * 30, 365);
+    var prazoEmDias = Math.min(prazo * 30, 365); // maximo de 365 dias
 
-    //var iofTotal = emprestimo * (iofDiario * prazoEmDias + iofFixo);
-    var iofTotal = emprestimo * (iofDiario * prazoEmDias);
+    var iofTotal = emprestimo * (iofDiario * prazoEmDias + iofFixo);
     var emprestimoLiquido = emprestimo - iofTotal - valorSeguro;
     var valorTotal = valorParcela * prazoAtual;
 
@@ -123,7 +114,7 @@ function calcularEmprestimoValorFixo(valorParcela, prazoAtual) {
     };
 }
 
-function recalcularParcelas() {
+function recalcularParcelas() { // funçao para calcular o valor do empréstimo baseado no prazo atualizado
     document.getElementById("error").innerHTML = ""; // Adicionado para limpar a mensagem de erro
 
     prazoRecalculado = parseFloat(document.getElementById("numParcelas").value);
@@ -139,14 +130,14 @@ function recalcularParcelas() {
     document.getElementById("recalcularValor").style.display = "block";
 }
 
-function recalcularValor() {
+function recalcularValor() { // funçao para  calcular o empréstimo baseado no valor da parcela atualizada
     document.getElementById("error").innerHTML = ""; // Adicionado para limpar a mensagem de erro
 
     var valorParcela = converterParaNumero(document.getElementById("valorParcela").value);
     var prazoAtual = prazoRecalculado || prazo;
     parcela = aposentadoria * margem;
 
-    if (valorParcela <= 14 || valorParcela > parcela) {
+    if (valorParcela <= 14 || valorParcela > parcela || isNaN(valorParcela)) {
         document.getElementById("error").innerHTML = "Por favor, insira um valor de parcela válido (valor minimo de R$15,00 e maximo de R$" + parcela.toLocaleString('pt-BR', {minimumFractionDigits: 2}) + ").";
         return;
     }
